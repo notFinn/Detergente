6582e12a91930a2ca384b2f97ba7c398675c29f2
notFinn <rodrigoburgos100@gmail.com>
2018-03-11 00:10:24 -0300

    Mostrar Carrito de compra

--- Detergente/Metodos/AgregarCarro.cs
+++ Detergente/Metodos/AgregarCarro.cs
@@ -11,15 +11,24 @@ namespace Detergente.Metodos
         public static string Agregar(Producto producto,string sesion) {
             contar = +1;
             List<Carrito> Lista;
+
             if (sesion == null)
                 Lista = new List<Carrito>();
             else {
                 Lista = JsonConvert.DeserializeObject<List<Carrito>>(sesion);
             }
 
-
             List<Producto> Pro = new List<Producto>();
             Pro.Add(producto);
+            foreach (var lpro in Lista)
+            {
+                if (lpro.IdProduco == producto.Id)
+                {
+                    lpro.Cantidad = lpro.Cantidad+1;
+                    return JsonConvert.SerializeObject(Lista, Formatting.Indented);
+                }
+                
+            }
             var carrito = new Carrito(contar, producto.Id, 1, DateTime.Now, producto);
             Lista.Add(carrito);
 
@@ -29,6 +38,40 @@ namespace Detergente.Metodos
         public static List<Carrito> Listar() {
 
             return null;
+        }
+        public static int ContarCarrito(string json) {
+
+            List<Carrito> Lista;
+            if (json == null)
+                return 0;
+            else {
+                    Lista = JsonConvert.DeserializeObject<List<Carrito>>(json);
+            }
+            int cont = 0;
+            foreach (var pro in Lista)
+            {
+                cont = cont + pro.Cantidad;
+            }
+
+            return cont;
+        }
+        public static int TotalizarCarrito(string json)
+        {
+
+            List<Carrito> Lista;
+            if (json == null)
+                return 0;
+            else
+            {
+                Lista = JsonConvert.DeserializeObject<List<Carrito>>(json);
+            }
+            int cont = 0;
+            foreach (var pro in Lista)
+            {
+                cont = (int)(cont + (pro.Cantidad*pro.Producto.Precio));
+            }
+
+            return cont;
         }
     }
 }
