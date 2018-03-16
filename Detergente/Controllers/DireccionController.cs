9eeb94171c46e92cdc6d2b596eaf536c29bdc380
notFinn <rodrigoburgos100@gmail.com>
2018-03-15 21:24:16 -0300

    Controller Direccion

---
+++ Detergente/Controllers/DireccionController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Detergente.Controllers
+{
+    public class DireccionController : Controller
+    {
+        // GET: Direccion
+        public ActionResult CalcularDistancia()
+        {
+            return View();
+        }
+    }
+}
